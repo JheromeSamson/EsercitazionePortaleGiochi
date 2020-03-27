3af8f17260499812e47e9445cef4fa3da8067ad2
jherome <jherome.samson90@gmail.com>
2020-03-27 23:14:45 +0100

    cad carousel bootstrap

--- src/app/service/util/salvaAccesso/accesso.service.ts
+++ src/app/service/util/salvaAccesso/accesso.service.ts
@@ -1,58 +1,51 @@
 import { Injectable } from '@angular/core';
 import { User } from 'src/app/model/user';
 import { UserService } from '../../user/user.service';
+import { Key } from 'protractor';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccessoService {
 
- userService:UserService;
+  userService: UserService;
 
- name:string;
- user:User;
- registrati:User[];
- appoggio;
+  name: string;
+  user: User;
+  registrati = [];
+  appoggio = [];
+
+  index: number;
 
   constructor(private userServ: UserService) {
     this.userService = userServ;
-   }
-
-  salvaRegistrazione(user: User){
-
-    if (!localStorage.getItem("users")){
-      localStorage.setItem("users", JSON.stringify(user));
-    }else if(localStorage.getItem("users")){
-      this.registrati =  JSON.parse(localStorage.getItem("users"));
-      this.appoggio = JSON.stringify(user)
-      //this.registrati.push(this.appoggio);
-      console.log(this.registrati);
-      //localStorage.setItem("users", JSON.stringify(this.registrati));
-    }
-    
-    return true;
-    
-
-
-/*
-    if(this.registrati("name") != user.getName() || localStorage.getItem("email") != user.getEmail() || localStorage.getItem("password") != user.getPassword())
-    {
-      this.userService.setUser(user);
-
-      localStorage.setItem("name", user.getName());
-      localStorage.setItem("email", user.getEmail());
-      localStorage.setItem("password", user.getPassword());
-      this.name = localStorage.getItem("name");
-
-      sessionStorage.setItem("name", this.name);
-      return true;
-    }else{ 
-      return false;}*/
   }
 
-  salvaLogin(email: string, password: string){
-    
-    if( localStorage.getItem("email") === email && localStorage.getItem("password") === password){
+  salvaRegistrazione(user: User) {
+
+    if (!localStorage.getItem("users")) {
+      localStorage.setItem("users", JSON.stringify(user));
+      return true;
+    }
+    else if (localStorage.getItem("users")) {
+    this.registrati = JSON.parse(localStorage.getItem("users"));
+      for(let i =0; i< this.registrati.length; i++){
+        let attualeRegistrato = JSON.parse(this.registrati[i]);
+        if(attualeRegistrato.password == user.getPassword && attualeRegistrato.email == user.getEmail){
+          return false;
+        }
+      }
+      this.appoggio.push(JSON.stringify(user));
+      localStorage.setItem("users", JSON.stringify(this.appoggio));
+      sessionStorage.setItem("name", user.getName());
+      return true;
+    }
+    else return false;
+  }
+
+  salvaLogin(email: string, password: string) {
+
+    if (localStorage.getItem("email") === email && localStorage.getItem("password") === password) {
 
       this.name = localStorage.getItem("name");
       sessionStorage.setItem("name", this.name);
@@ -61,7 +54,8 @@ export class AccessoService {
 
       this.userService.setUser(this.user);
       return true;
-    }else{ 
-      return false;}
+    } else {
+      return false;
+    }
   }
 }
